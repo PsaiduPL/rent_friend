744c1dd91f8a9d5c0ee16e6e30e9ac44a2c025bb
Psaidu <ekurdziel@student.agh.edu.pl>
2025-08-04 18:10:17 +0200

    feat : profile repo created

---
+++ backend/rentfriend/src/main/java/org/rentfriend/entity/Profile.java
@@ -0,0 +1,38 @@
+package org.rentfriend.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profiles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Profile {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false,
+      columnDefinition = "TEXT")
+  private String description;
+
+  @Column(nullable = false)
+  String city;
+
+  @Column(nullable = false)
+
+  Integer age;
+  @JsonIgnore
+  @OneToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", referencedColumnName = "id",
+      nullable = false, insertable = true, updatable = false
+      , unique = true)
+  MyUser user;
+}
